932f918bc346f5c44617ef22e1b91614f5e8eca4
IliaGlaz <glazterix@gmail.com>
2023-06-23 23:39:52 +0300

    leet-code project

--- simple-project/src/task8/Runner.java
+++ simple-project/src/task8/Runner.java
@@ -1,28 +1,12 @@
 package task8;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Runner {
 
-    public static void main(String[] args) {
-        int[] nums = new int[]{3,3};
-        int target = 6;
-        int[] ints = twoSum(nums, target);
-        for (int i = 0; i < ints.length; i++) {
-            System.out.println(ints[i]);
+    public static void main(String[] args) throws InterruptedException {
+        int i = 0;
+        while(true) {
+            Thread.sleep(1000);
+            System.out.println(i++);
         }
-    }
-
-    public static int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> secondInt = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (secondInt.containsKey(nums[i])) {
-                return new int[]{secondInt.get(nums[i]), i};
-            }
-            secondInt.put(target - nums[i], i);
-        }
-        return new int[]{};
     }
 }
