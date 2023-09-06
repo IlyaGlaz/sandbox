304e4cca876e3d1ca92b8876879bef391afd7b72
IliaGlaz <glazterix@gmail.com>
2023-09-07 00:48:58 +0300

    firstkotlin 7/9

--- first-kotlin/src/main/kotlin/joyofkotlin/ch03/Task01.kt
+++ first-kotlin/src/main/kotlin/joyofkotlin/ch03/Task01.kt
@@ -9,5 +9,8 @@ fun square(n: Int) = n * n
 
 fun triple(n: Int) = n * 3
 
-fun compose(a: (Int) -> Int, b: (Int) -> Int): (Int) -> Int =
-    {a(b(it))}
+fun compose(
+    a: (Int) -> Int,
+    b: (Int) -> Int
+): (Int) -> Int =
+    { a(b(it)) }
--- first-kotlin/src/main/kotlin/kotlininaction/ch02/Color.kt
+++ first-kotlin/src/main/kotlin/kotlininaction/ch02/Color.kt
@@ -3,7 +3,8 @@ package kotlininaction.ch02
 enum class Color(
     val r: Int, val g: Int, val b: Int
 ) {
-    RED(255, 0, 0), ORANGE(255, 165, 0),
+    RED(255, 0, 0),
+    ORANGE(255, 165, 0),
     YELLOW(255, 255, 0);
 
     fun rgb() = (r * 256 + g) * 256 + b
